27c3af8193e512dd54ac7b228ecfefe6fffbff44
oivansoares <ivanddd30@gmail.com>
2026-03-11 22:11:05 -0300

    ult

---
+++ CONDIC/if e else.py
@@ -0,0 +1,17 @@
+nota = 5
+media = 6
+nota_recuperacao = 2
+nota_final = 10
+
+if nota >= media:
+    print("Aprovado😁👍")
+elif nota < media: # aluno na recuperação
+    print("Recuperação😕")
+    if nota_recuperacao >= media:
+        print("Aprovado na recuperação😁👍")
+    if nota_recuperacao < media:
+        print("Voce vai para prova final")
+    if nota_final >= media:
+        print("Aprovado na prova final😁👍")
+    else:
+        print("Reprovado na prova final😞👎 ")
