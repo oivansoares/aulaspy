e496e271146c3b4a6c4974cd710f73da70e6e8dd
oivansoares <ivanddd30@gmail.com>
2026-03-18 18:53:20 -0300

    len

--- ex de for.py
+++ ex de for.py
@@ -2,4 +2,4 @@ notas = [ 9.1 , 9.5 , 6.7 , 10.0]
 disciplinas = ( "Propotipagem" , "Interface" , "Prompts" , "Programação")
 
 for i in range(len(notas)):
-    print(f"A disciplina {disciplinas[i]} teve a nota : {notas[i]}")
+    print(f"A disciplina {disciplinas[i]} teve a nota : {notas[i]}") #A função len() retorna o número de elementos em uma lista ou tupla, e o range() gera uma sequência de números de 0 até o número fornecido (neste caso, o comprimento da lista de notas). Assim, o loop percorre cada índice da lista de notas e imprime a disciplina correspondente e a nota associada.
