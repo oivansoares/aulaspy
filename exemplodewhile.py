5ea95578e92fe606c5b0f4577d09a17c29969f2e
oivansoares <ivanddd30@gmail.com>
2026-03-21 21:45:46 -0300

    .

---
+++ exemplodewhile.py
@@ -0,0 +1,10 @@
+n = 20
+while n>=0:
+    if n % 2 == 1:
+        n = n - 1
+        continue
+    print(n)
+    n = n - 1
+    
+# O código acima imprime os números pares de 20 a 0, utilizando o comando continue para pular os números ímpares.
+  
